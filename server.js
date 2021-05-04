538649454f739444f41de14db181c14e7c6aef7b
AkhilNarayan123 <laxminarayanaakhil123@gmail.com>
2021-05-04 16:05:05 +0530

    changes

--- server.js
+++ server.js
@@ -10,4 +10,6 @@ app.get('/', function(req, res){
 app.use(express.static(__dirname));
 
 var port= process.env.PORT  || 3000;
-app.listen(port);
+app.listen(port,function cb()
+{console.log("http://localhost:"+port)
+});
