945f95d8a9566be33e41031db3b85096f61c9370
AkhilNarayan123 <laxminarayanaakhil123@gmail.com>
2021-05-05 15:36:50 +0530

    server routing

--- server.js
+++ server.js
@@ -1,24 +1,30 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
-console.log(__dirname)
-app.get('/google', function(req, res){
-    res.sendFile(__dirname+ '/google.html');
-})
 
 app.get('/', function(req, res){
-    res.sendFile(__dirname+ '/apple.html');
+   res.sendFile(__dirname+ '/frontend/html/resume.html'); 
 })
 
-app.get('/resume', function(req, res){
-    res.sendFile(__dirname+ '/resume.html');
+app.use(express.static(__dirname+'/frontend'));
+app.get('/:page', function(req, res){
+    var ext = path.extname(req.params.page);
+    // console.log(ext);
+    if(ext=="")
+    res.sendFile(__dirname+ '/frontend/html/'+ req.params.page+".html");
+    //else  res.sendFile(__dirname+ '/frontend/'+ req.params.page);
 })
 
-app.get('/', function(req, res){
-    res.sendFile(__dirname+ '/resume.html');
-})
 
-app.use(express.static(__dirname));
+// app.get('/resume', function(req, res){
+//     res.sendFile(__dirname+ '/resume.html');
+// })
+
+// app.get('/', function(req, res){
+//     res.sendFile(__dirname+ '/resume.html');
+// })
+
 
 var port= process.env.PORT  || 3000;
 app.listen(port,function cb()
