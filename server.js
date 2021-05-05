3959ed858fbacad5b439622c91d5ebf9f50004d1
AkhilNarayan123 <laxminarayanaakhil123@gmail.com>
2021-05-05 09:00:43 +0530

    serve

--- server.js
+++ server.js
@@ -2,9 +2,20 @@ var express = require('express');
 var app = express();
 
 console.log(__dirname)
+app.get('/google', function(req, res){
+    res.sendFile(__dirname+ '/google.html');
+})
+
 app.get('/', function(req, res){
     res.sendFile(__dirname+ '/apple.html');
-    
+})
+
+app.get('/resume', function(req, res){
+    res.sendFile(__dirname+ '/resume.html');
+})
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname+ '/resume.html');
 })
 
 app.use(express.static(__dirname));
